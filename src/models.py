a02e4a11565734679fbda2480a3504ba841429b9
Gabriel Pezo Rojas <156217567+gabrielpezo@users.noreply.github.com>
2024-04-27 17:10:43 +0000

    data modelling final

--- src/models.py
+++ src/models.py
@@ -7,26 +7,65 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String (250), unique=True, nullable=False)
+    password = Column(String(200), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Photographer(Base):
+    __tablename__ = 'photographer'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    email = Column(String (250), unique=True, nullable=False)
+    password = Column(String(200), nullable=False)
+    photos_id = Column(Integer, ForeignKey('photos.id'))
+    uploaded_photos = relationship ("Photos")
 
-    def to_dict(self):
-        return {}
+class Favourites(Base):
+    __tablename__ = 'favourites'
+    id = Column(Integer, primary_key=True)
+    photographer_id = Column(Integer, ForeignKey('photographer.id'))
+    photographer = relationship(Photographer)
+    photo_id = Column(Integer, ForeignKey('photos.id'))
+    photo = relationship("Photos")
+
+class Photos(Base):
+    __tablename__ = 'photos'
+    id = Column(Integer, primary_key=True)
+    picture = Column(String(100), nullable=False)
+    price = Column(String(20), nullable=False)
+
+class FAQ (Base):
+    __tablename__ = 'faq'
+    id = Column(Integer, primary_key=True)
+    questions = Column(String(500), nullable=False)
+    user_id = (Integer, ForeignKey("user.id"))
+    user = relationship(User)
+
+class Comments (Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    comments = Column(String(1000), nullable=False)
+    user_id = (Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Cart(Base):
+    __tablename__ = 'cart'
+    id = Column(Integer, primary_key=True)
+    user_id = (Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    photo_id = Column(Integer, ForeignKey('photos.id'))
+    photo = relationship(Photos)
+     
+class Followers(Base):
+    __tablename__ = "followers"
+    id = Column(Integer, primary_key=True)
+    user_id = (Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    photographer_id = Column(Integer, ForeignKey('photographer.id'))
+    photographer = relationship(Photographer)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
